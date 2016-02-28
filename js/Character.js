569afe2d6e3a5f3d75558a5b0246f4b644384477
dean99dean <dean99dean@gmail.com>
2016-02-27 20:56:39 -0500

    debug:

--- js/Character.js
+++ js/Character.js
@@ -15,7 +15,7 @@ class Character extends Obj{
   //   return gridPixel;
   // }
 
-  function initializeCharacter(){
+ initializeCharacter(){
     ctx.beginPath();
     ctx.arc(120, 900, 17, 0, Math.PI*2, true);//character size
     ctx.fillStyle = "#0095DD";
@@ -23,31 +23,33 @@ class Character extends Obj{
     ctx.closePath();
   }
 
-  function draw() {
+   draw() {
     this.clear();
     var dy = -1;
     y += dy;
   }
 
-  setInterval(draw ,20);//check every 20ms
+  // setInterval(draw ,20);//check every 20ms
 
-  function convertPixel(counter){
+   convertPixel(counter){
     var H_BLOCKS = 14;
     var H_DIMENTION = 256;
     var V_BLOCKS = 25;
     var V_DIMENTION = 960;
-    var gridPixel.x = counter.GridX * (H_DIMENTION / H_BLOCKS);
-    var gridPixel.y = (counter.GridY+1) * (V_DIMENTION / V_BLOCKS);
+    var gridPixel = {
+      x: counter.GridX * (H_DIMENTION / H_BLOCKS),
+      y: (counter.GridY + 1) * (V_DIMENTION / V_BLOCKS)
+    }
     return gridPixel;
   }
 
 
-  function checkCollision(portals, enemies, items){
+   checkCollision(portals, enemies, items){
     var collisionType;
     //loop through every object in the map
-    for(var i = 0; i < portal.length; i++){
-      var pixel = convertPixel(portal[i]);//in pixel
-      if((this.currY == pixel.y) && (this.currX == pixel.x){
+    for(var i = 0; i < portals.length; i++){
+      var pixel = convertPixel(portals[i]);//in pixel
+      if((this.currY == pixel.y) && (this.currX == pixel.x)){
         this.clear();
         collsionType = "portal";
         currX = portal[i+1].GridX;//in grid
@@ -63,14 +65,14 @@ class Character extends Obj{
     for(var i = 0; i < enemies.length; i++){
       var pixel = convertPixel(enemies[i]);//in pixel
       // if(this.currY > i.y && this.currY < i.y - 38.4 && (currX > i.x && currX < i.x + 18.28){
-      if((this.currY == pixel.y) && (this.currX == pixel.x){
+      if((this.currY == pixel.y) && (this.currX == pixel.x)){
         collsionType = "enemies";
         document.reload();
       }
     }
     for(var i = 0; i < items.length; i++){
       var pixel = convertPixel(items[i]);//in pixel
-      if((this.currY == pixel.y) && (this.currX == pixel.x){
+      if((this.currY == pixel.y) && (this.currX == pixel.x)){
         collsionType = "items";
         itemCollected ++;
         console.log(itemCollected);
@@ -78,16 +80,16 @@ class Character extends Obj{
     }
   }
 
-  setInterval(checkCollision ,20);//check every 20ms
-  document.addEventListener("keydown", changeState);
+  // setInterval(checkCollision ,20);//check every 20ms
+  // document.addEventListener("keydown", changeState);
 
 
-  function changeState(e){
+   changeState(e){
     //register a event listener
     var key = e.keyCode ? e.keyCode : e.which;
     if (key == 38) {
       console.log("You Pressed the fking Space");
-      switch(state):
+      switch(state){
         case 0:
           state++;
           console.log("White");
@@ -96,7 +98,13 @@ class Character extends Obj{
           state--;
           console.log("Black");
           break;
+      }
+        
      }
   }
 }
-}
+
+module.exports = Character;
+
+
+
